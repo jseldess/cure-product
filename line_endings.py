dbd3c8344ddec212ac4f0c32aa0392bf77f1d9e4
Jesse Seldess <jesse@cockroachlabs.com>
2016-04-09 21:58:24 -0400

    english/italian labels

--- line_endings.py
+++ line_endings.py
@@ -8,29 +8,11 @@ e = "ere"
 f = "o"
 g = "en"
 
-def english_sonnets():
-    lines_seen = set()
-    f1 = '../text/source.txt'
-    with open('../sonnets/2english_sonnets.txt', 'w') as f2:
-        f2.write('a=' + a + '\n' + 'b=' + b + '\n' + 'c=' + c + '\n' + 'd=' + d + '\n' + 'e=' + e + '\n' + 'f=' + f + '\n' + 'g=' + g + '\n\n')
-        while True:
-            for ending in [a, b, a, b, c, d, c, d, e, f, e, f, g, g]:
-                for line in open(f1):
-                    if re.search('.' + ending + '$', line) is not None and len(line) < 100:
-                        if line not in lines_seen:
-                            f2.write(line)
-                            lines_seen.add(line)
-                            break
-                else:
-                    return
-
-            f2.write('\n')
-
 def italian_sonnets():
     lines_seen = set()
-    f1 = '../text/source.txt'
-    with open('../sonnets/2italian_sonnets.txt', 'w') as f2:
-        f2.write('a=' + a + '\n' + 'b=' + b + '\n' + 'c=' + c + '\n' + 'd=' + d + '\n' + 'e=' + e + '\n\n')
+    f1 = 'source.txt'
+    with open('sonnets.txt', 'w') as f2:
+        f2.write('ITALIAN SONNETS:' + ' (a=' + a + ', ' + 'b=' + b + ', ' + 'c=' + c + ', ' + 'd=' + d + ', ' + 'e=' + e + ')\n\n')
         while True:
             for ending in [a, b, b, a, a, b, b, a, c, d, e, c, d, e]:
                 for line in open(f1):
@@ -44,6 +26,23 @@ def italian_sonnets():
 
             f2.write('\n')
 
+def english_sonnets():
+    lines_seen = set()
+    f1 = 'source.txt'
+    with open('sonnets.txt', 'a') as f2:
+        f2.write('\n' + 'ENGLISH SONNETS:' + ' (a=' + a + ', ' + 'b=' + b + ', ' + 'c=' + c + ', ' + 'd=' + d + ', ' + 'e=' + e + ', ' + 'f=' + f + ', ' + 'g=' + g + ')\n\n')
+        while True:
+            for ending in [a, b, a, b, c, d, c, d, e, f, e, f, g, g]:
+                for line in open(f1):
+                    if re.search('.' + ending + '$', line) is not None and len(line) < 100:
+                        if line not in lines_seen:
+                            f2.write(line)
+                            lines_seen.add(line)
+                            break
+                else:
+                    return
+            f2.write('\n')
 
-english_sonnets()
+
 italian_sonnets()
+english_sonnets()
